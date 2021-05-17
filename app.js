be5f9744169de28cb46f31ecb3dba0ed78114c4c
AryanPatel <asp6304@gmail.com>
2021-05-17 17:25:02 +0530

    try

--- app.js
+++ app.js
@@ -22,6 +22,13 @@ app.use(morgan('dev'));
 
 console.log("Listening");
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 
 app.get('/', (req, res) => {
     res.redirect('/home');
