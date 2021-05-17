1b6dc3abf4ca4b1f2a90fac9b9820c60633fe486
AryanPatel <asp6304@gmail.com>
2021-05-17 11:48:36 +0530

    try

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 require( 'dotenv' ).config();
-const blogRoutes = require('./routes/blogRoutes');
+const blogRoutes = require('./routes/blogroutes');
 
 // console.log(process.env);
 
